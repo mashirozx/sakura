8f09dfe8ab8d33ef31f0060ffb3fe1fda1dacad1
Ekkles <41195266+EkklesN@users.noreply.github.com>
2019-10-25 15:07:57 +1300

    Update footer.php

--- footer.php
+++ footer.php
@@ -58,7 +58,7 @@
 			<img src="<?php echo $ava ?>">
 		</div>
 		<p style="text-align: center; color: #333; font-weight: 900; font-family: 'Ubuntu', sans-serif; letter-spacing: 1.5px">Contact me</p>
-<p style="text-align: center; word-spacing: 20px;"> <a href="https://t.me/EkklesN" class="social-lofter" target="_blank" style="color: #00aced" rel="noopener noreferrer nofollow"></a> <a onclick="mail_me()" class="fa fa-envelope" style="color: #ffbf00" rel="nofollow"></a></p>
+		<li><a href="https://twitter.com/EkklesN" target="_blank" class="social-wangyiyun" title="Twitter"><img src="https://cdn.jsdelivr.net/gh/moezx/cdn@3.1.9/img/Sakura/images/sns/twitter.png"/></a></li>
 		<div class="m-search">
 			<form class="m-search-form" method="get" action="<?php echo home_url(); ?>" role="search">
 				<input class="m-search-input" type="search" name="s" placeholder="<?php _e('搜索...', 'akina') ?>" required>
