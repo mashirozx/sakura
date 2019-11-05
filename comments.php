e2f9c3d0df90ce5c921aace13ae99f98bcb9c4b4
Ekkles <41195266+EkklesN@users.noreply.github.com>
2019-11-06 11:39:18 +1300

    Update comments.php

--- comments.php
+++ comments.php
@@ -64,7 +64,7 @@
                         <div id="upload-img-show"></div>
                         <!--插入表情面版-->
                         <p id="emotion-toggle" class="no-select">
-                            <span class="emotion-toggle-off">' . __("表情面版", "sakura")/*表情面版*/ . '</span>
+                            <span class="emotion-toggle-off">' . __("插入表情", "sakura")/*表情面版*/ . '</span>
                             <span class="emotion-toggle-on">' . __("收起", "sakura")/*收起*/ . '</span>
                         </p>
                         <div class="emotion-box no-select">
