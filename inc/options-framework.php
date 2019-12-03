eda14ef1754810f40a405e05e4a1b2a4d823c432
mashiro <moezhx@outlook.com>
2019-12-03 22:52:50 +0800

    fix

--- inc/options-framework.php
+++ inc/options-framework.php
@@ -262,7 +262,6 @@ if ( !function_exists( 'optionsframework_page' ) ) :
 function optionsframework_page() { ?>
 
 	<div id="optionsframework-wrap" class="wrap">
-    <?php screen_icon( 'themes' ); ?>
     <h2 class="nav-tab-wrapper">
         <?php echo optionsframework_tabs(); ?>
     </h2>
