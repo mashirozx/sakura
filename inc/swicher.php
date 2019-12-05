116d03974396d2c54cf5493d201e57270c9858c0
spirit <spirit1431007@gmail.com>
2019-12-05 20:09:41 +0800

    fix

--- inc/swicher.php
+++ inc/swicher.php
@@ -13,7 +13,7 @@ mashiro_option.site_name = "<?php echo akina_option('site_name', ''); ?>";
 mashiro_option.author_name = "<?php echo akina_option('author_name', ''); ?>";
 mashiro_option.template_url = "<?php echo get_template_directory_uri(); ?>";
 mashiro_option.site_url = "<?php echo site_url(); ?>";
-mashiro_option.qq_api_url = "<?php echo rest_url('sakura/v1/qqinfo/json'); ?>""; 
+mashiro_option.qq_api_url = "<?php echo rest_url('sakura/v1/qqinfo/json'); ?>"; 
 // mashiro_option.qq_avatar_api_url = "https://api.2heng.xin/qqinfo/";
 mashiro_option.live_search = <?php if ( akina_option('live_search') ){ echo 'true'; } else { echo 'false'; } ?>;
 
