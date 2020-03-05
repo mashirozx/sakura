c0f0990ff037f377f17dd0206192caed658705b2
Spirit <spirit1431007@gmail.com>
2020-03-06 00:08:36 +0800

    perf: optmize

--- inc/api.php
+++ inc/api.php
@@ -402,7 +402,7 @@ function get_qq_avatar(){
     $encrypted=$_GET["qq"];
     if(isset($encrypted)){
         $iv = str_repeat($sakura_privkey, 2);
-        $encrypted = urldecode(base64_decode($encrypted));
+        $encrypted = base64_decode(urldecode($encrypted));
         $qq_number = openssl_decrypt($encrypted, 'aes-128-cbc', $sakura_privkey, 0, $iv);
         preg_match('/^\d{3,}$/', $qq_number, $matches);
         $imgurl='https://q2.qlogo.cn/headimg_dl?dst_uin='.$matches[0].'&spec=100';
