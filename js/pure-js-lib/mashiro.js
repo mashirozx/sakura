d4d331d4b5e37eba5f7fc848bb09259aec32f74c
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2022-06-27 18:03:54 +0800

    Delete js/pure-js-lib directory

--- js/pure-js-lib/mashiro.js
+++
@@ -1,7 +0,0 @@
-function hasClass(el, className) {
-    if (el.classList)
-        var e = el.classList.contains(className);
-    else
-        var e = new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
-    return e;
-}
